e50f501ce3af41dde0b6ae4d83cd65d14cbf841b
Aarti626 <43464568+Aarti626@users.noreply.github.com>
2018-10-17 11:36:18 +0530

    Create aap.py

---
+++ aap.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import os
+# from heapq import nlargest
+# from heapq import nsmallest
+
+app = Flask(__name__)
+# global identifier
+
+
+@app.route("/")
+def index():
+   print("Render")
+   return render_template("index.html")
+
+@app.route("/sum",methods=["GET"])
+def sum():
+#     num1=request.args.get("num1", "")
+#     num2=request.args.get("num2", "")  
+#     sum = int(num1) + int(num2)
+    
+# Display the sum
+#     print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+    return render_template("login.html")
+    
+        
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+    # sentiment_anlysis()
