6d1cff3a678ff6086f948bae373206bfbe811597
Aarti626 <43464568+Aarti626@users.noreply.github.com>
2018-10-18 14:37:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def sum():
         
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
     # sentiment_anlysis()
