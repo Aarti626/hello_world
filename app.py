a53972bdcf9e83fd3a36a9111319748367ac3cbe
Aarti626 <43464568+Aarti626@users.noreply.github.com>
2018-10-18 13:05:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
    print("Render")
-   return render_template("index.html")
+   return render_template("templates/index.html")
 
 @app.route("/sum",methods=["GET"])
 def sum():
