817c80b16da6fc713bd5cd6111a0b5b65806dc29
Aarti626 <43464568+Aarti626@users.noreply.github.com>
2018-10-19 11:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import os
+from os import listdir, rename, remove
+import sys
 
 # from heapq import nlargest
 # from heapq import nsmallest
@@ -13,7 +15,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route("/")
 def index():
    print("Render")
-   return render_template("templates/index.html")
+   return render_template("./templates/index.html")
 
 @app.route("/sum",methods=["GET"])
 def sum():
@@ -24,7 +26,7 @@ def sum():
 # Display the sum
 #     print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
-    return render_template("templates/login.html")
+    return render_template("./templates/login.html")
     
         
 if __name__ == "__main__":
